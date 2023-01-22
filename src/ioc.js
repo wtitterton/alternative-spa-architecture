02b4f6fa4d9b4701c430a5b462c25d267545a745
William Titterton <william.titterton@attractions.io>
2023-01-22 14:12:31 +0000

    Registered necessary classes in container

--- src/ioc.js
+++ src/ioc.js
@@ -1,1 +1,12 @@
 import { Container } from 'inversify'
+import {HttpGateway} from './gateways';
+import {RoutingState, Router, RouterGateway} from './routing'
+export const container = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Transient'
+})
+
+container.bind(HttpGateway).to(HttpGateway)
+container.bind(RouterGateway).to(RouterGateway).inSingletonScope()
+container.bind(RoutingState).to(RoutingState).inSingletonScope()
+container.bind(Router).to(Router).inSingletonScope()
