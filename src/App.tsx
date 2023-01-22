4b009bd37d7877e9abd94fec57b20aa077b8276c
William Titterton <william.titterton@attractions.io>
2023-01-22 11:02:48 +0000

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App w-screen h-screen">
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
